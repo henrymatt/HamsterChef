b5db7ba8c9469159855769f8c4190903e261214c
harrison531 <harrisongardner531@gmail.com>
2023-03-10 22:52:01 -0700

    Lighting changes and added a blob

--- Assets/Shaders/CustomLighting.hlsl
+++ Assets/Shaders/CustomLighting.hlsl
@@ -1,6 +1,8 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
 #define CUSTOM_LIGHTING_INCLUDED
 
+#pragma multi_compile _ _ADDITIONAL_LIGHTS
+
 struct CustomLightingData {
 	float3 globalLighting;
 
@@ -12,11 +14,11 @@ struct CustomLightingData {
 
 #ifndef SHADERGRAPH_PREVIEW
 float3 CustomLightHandling(CustomLightingData d, Light light) {
-	float3 radiance = light.color;
+	float3 radiance = float3(clamp(light.color.r, 0, 1), clamp(light.color.g, 0, 1), clamp(light.color.b, 0, 1));
 
 	float diffuse = saturate(dot(d.normalWS, light.direction));
 
-	float3 color = d.albedo * radiance * diffuse + d.globalLighting;
+	float3 color = d.albedo * radiance * diffuse;
 
 	return color;
 }
@@ -27,13 +29,15 @@ float3 CalculateCustomLighting(CustomLightingData d) {
 #ifdef SHADERGRAPH_PREVIEW
 	float3 lightDir = float3(0.5, 0.5, 0);
 	float intensity = saturate(dot(d.normalWS, lightDir));
-	return d.albedo * intensity + d.globalLighting;
+	return clamp(d.albedo * intensity, 0, 1);
 #else
 	Light mainLight = GetMainLight();
 
 	float color = 0;
 
 	color += CustomLightHandling(d, mainLight);
+
+	color = clamp(color, max(max(mainLight.color.r, mainLight.color.g), mainLight.color.b) * d.globalLighting, 1);
 
 	return color;
 #endif
